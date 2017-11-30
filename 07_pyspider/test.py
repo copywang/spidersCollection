e4e0065b3109b93e0a95a04e443814da2e302c35
copywang <1670162477@qq.com>
2017-11-30 10:55:43 +0800

    update to 07

---
+++ 07_pyspider/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+test = 'https://www.nvshens.com/img.html?img=https://t1.onvshen.com:85/gallery/25329/24707/005.jpg'
+
+a = test.split('/')[-1]
+print(a)
